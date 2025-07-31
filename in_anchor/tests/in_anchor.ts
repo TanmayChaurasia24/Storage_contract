d04188704f7d75346b2fdc99cb79ec33dd78691a
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-08-01 01:19:53 +0530

    wriiten tests for anchor

--- in_anchor/tests/in_anchor.ts
+++ in_anchor/tests/in_anchor.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { InAnchor } from "../target/types/in_anchor";
+// @ts-ignore: Import type may not be generated yet
+import type { InAnchor } from "../target/types/in_anchor";
+import { assert } from "chai";
 
 describe("in_anchor", () => {
   // Configure the client to use the local cluster.
@@ -9,8 +11,27 @@ describe("in_anchor", () => {
   const program = anchor.workspace.inAnchor as Program<InAnchor>;
 
   it("Is initialized!", async () => {
+    const dataAccount = anchor.web3.Keypair.generate();
+    const payer = anchor.web3.Keypair.generate();
+    
+    const airdrop_txn = await anchor.getProvider().connection.requestAirdrop(payer.publicKey, 1000000000000000);
+    await anchor.getProvider().connection.confirmTransaction(airdrop_txn);
+    
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx_init = await program.methods.initialize("Tanmay").accounts({
+      dataAccount: dataAccount.publicKey,
+      payer: payer.publicKey
+    }).signers([payer, dataAccount]).rpc();
+
+    let dataAccountInfo = await program.account.accountFormat.fetch(dataAccount.publicKey);
+    assert(dataAccountInfo.name, "Tanmay");
+
+    await program.methods.update("Tanmay Kumar Chaurasia").accounts({
+      dataAccount: dataAccount.publicKey,
+      payer: payer.publicKey,
+    }).signers([payer]).rpc();
+
+    dataAccountInfo = await program.account.accountFormat.fetch(dataAccount.publicKey);
+    assert(dataAccountInfo.name, "Tanmay Kumar Chaurasia");
   });
 });
